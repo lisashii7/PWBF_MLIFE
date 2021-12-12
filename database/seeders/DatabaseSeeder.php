28cb9335dc0bec9e0190d2f5de6ff38350ba404b
tommyrachmadiono <tommyrachmadiono@gmail.com>
2021-12-12 20:15:18 +0700

    Update faker in seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -1,13 +1,15 @@
 <?php
 
 namespace Database\Seeders;
+
 use Illuminate\Support\Facades\Hash;
 
 use Illuminate\Database\Seeder;
 use App\Models\Santri;
 use App\Models\Pengurus;
 use App\Models\Buku;
-
+use App\Models\DetailPeran;
+use App\Models\Peran;
 
 class DatabaseSeeder extends Seeder
 {
@@ -22,15 +24,25 @@ class DatabaseSeeder extends Seeder
 
         $password = Hash::make("pengurus123");
         Pengurus::create([
-            "nama_pengurus"=>"admin",
-            "gender"=>"Laki-Laki",
-            "hp"=>"08231457897",
-            "email"=>"admin@gmail.com",
-            "password"=>$password,
-            "aktif"=>"Aktif"
+            "nama_pengurus" => "admin",
+            "gender" => "Laki-Laki",
+            "hp" => "08231457897",
+            "email" => "admin@gmail.com",
+            "password" => $password,
+            "aktif" => "Aktif"
 
         ]);
         Buku::factory(20)->create();
+        Peran::factory(5)->create();
 
+        $pengurus = Pengurus::first();
+        $perans = Peran::all();
+
+        foreach ($perans as $peran) {
+            DetailPeran::firstOrCreate([
+                'id_peran' => $peran->id_peran,
+                'id_pengurus' => $pengurus->id_pengurus
+            ]);
+        }
     }
 }
