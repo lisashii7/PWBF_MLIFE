1369a6339d7fc2b5d4e4dc3293169c696ebe3cbc
tommyrachmadiono <tommyrachmadiono@gmail.com>
2021-12-12 20:34:32 +0700

    Create model, migration, and faker data for table Bab

---
+++ app/Models/Bab.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Bab extends Model
+{
+    use HasFactory;
+
+    protected $table = "bab";
+    protected $primaryKey = "id_bab";
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Bab;
 use Illuminate\Support\Facades\Hash;
 
 use Illuminate\Database\Seeder;
@@ -10,6 +11,7 @@ use App\Models\Pengurus;
 use App\Models\Buku;
 use App\Models\DetailPeran;
 use App\Models\Peran;
+use Faker\Factory as Faker;
 
 class DatabaseSeeder extends Seeder
 {
@@ -20,6 +22,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        $faker = Faker::create('id_ID');
+
         Santri::factory(20)->create();
 
         $password = Hash::make("pengurus123");
@@ -37,11 +41,20 @@ class DatabaseSeeder extends Seeder
 
         $pengurus = Pengurus::first();
         $perans = Peran::all();
-
         foreach ($perans as $peran) {
             DetailPeran::firstOrCreate([
                 'id_peran' => $peran->id_peran,
                 'id_pengurus' => $pengurus->id_pengurus
+            ]);
+        }
+
+        $books = Buku::all();
+        foreach ($books as $book) {
+            Bab::firstOrCreate([
+                'id_buku' => $book->id_buku,
+                'bab' => $faker->text(50),
+                'judul' => $faker->text(100),
+                'keterangan' => $faker->sentence,
             ]);
         }
     }
